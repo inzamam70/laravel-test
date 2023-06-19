74448604802382532b7f9d2671e0aeb522a8c9e9
saroaremon <saroar01834@gmail.com>
2023-06-19 13:20:26 +0600

    migration

--- config/database.php
+++ config/database.php
@@ -62,6 +62,26 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        
+        'seip12' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('SEIP12_DB_HOST', '127.0.0.1'),
+            'port' => env('SEIP12_DB_PORT', '3306'),
+            'database' => env('SEIP12_DB_DATABASE', 'forge'),
+            'username' => env('SEIP12_DB_USERNAME', 'forge'),
+            'password' => env('SEIP12_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
---
+++ resources/views/form.blade.php
@@ -0,0 +1,41 @@
+<x-sg-master>
+<!-- Dashboard content -->
+<x-sg-card>
+<x-slot name="heading">
+Create Slider
+</x-slot>
+<x-slot name="body">
+        <form action="#" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input name="title" type="text" class="form-control" placeholder="Enter Title">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Caption</label>
+                        <input name="caption" type="text" class="form-control" placeholder="Enter Caption">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Alt</label>
+                        <input name="alt" type="text" class="form-control" placeholder="Alter">
+                    </div>
+                    <div class="form-group">
+                        <label>Upload Picture</label>
+                        <input type="file" name="image" class="form-control" placeholder="Upload Picture">
+                    </div>
+
+                    <div class="d-flex justify-content-start align-items-center">
+                        {{-- <button type="submit" class="btn btn-light legitRipple">Cancel</button> --}}
+                        <x-sg-custom-pink-color/>
+                        <button type="submit" class="btn bg-blue ml-3 legitRipple">Submit <i class="icon-paperplane ml-2"></i></button>
+                    </div>
+                </form>
+
+</x-slot>
+<x-slot name="cardFooterCenter">
+    End
+</x-slot>
+</x-sg-card>
+<!-- /dashboard content -->
+</x-sg-master>
